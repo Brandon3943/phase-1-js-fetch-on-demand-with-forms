b10ac308f16074633478215b8e807f51ecc6f8dc
root <root@Ty-Laptop.localdomain>
2022-11-25 19:08:00 -0500

    Completed Assignemnt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
+    let form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = document.querySelector('input#searchByID');
+        
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+          .then(responce => responce.json())
+          .then(data => {
+            const title = document.querySelector('#movieDetails h4');
+            const summary = document.querySelector('#movieDetails p');
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+          })
+    
+    });
+
 }
+  
+
 
 document.addEventListener('DOMContentLoaded', init);
